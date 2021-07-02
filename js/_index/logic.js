893babfbe0e45f891d0d180bce2406a86e871d26
Jason McNeill <vrtjason@gmail.com>
2021-07-01 20:18:30 -0700

    Update UI

--- js/_index/logic.js
+++ js/_index/logic.js
@@ -2,24 +2,32 @@
 
   function selectSendVia() {
     const sendToLabel = document.querySelector("#sendToLabel");
+    const containerName = document.querySelector("#containerName");
     const containerSms = document.querySelector("#containerSendToSms");
     const containerEmail = document.querySelector("#containerSendToEmail");
     const containerQRCode = document.querySelector("#containerSendToQRCode");
+    const containerTagWithLocation = document.querySelector("#containerTagWithLocation");
     const sendvia = getSendVia();
 
+    containerName.classList.add("d-none");
     sendToLabel.classList.add("d-none");
     containerSms.classList.add("d-none");
     containerEmail.classList.add("d-none");
     containerQRCode.classList.add("d-none");
+    containerTagWithLocation.classList.add("d-none");
 
     switch(sendvia) {
       case "sms":
+        containerName.classList.remove("d-none");
         sendToLabel.classList.remove("d-none");
         containerSms.classList.remove("d-none");
+        containerTagWithLocation.classList.remove("d-none");
         break;
       case "email":
+        containerName.classList.remove("d-none");
         sendToLabel.classList.remove("d-none");
         containerEmail.classList.remove("d-none");
+        containerTagWithLocation.classList.remove("d-none");
         break;
       case "qrcode":
         sendToLabel.classList.add("d-none");
