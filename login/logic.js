609f27c858493e5ae765a42be50f6cc6829f2def
Jason McNeill <vrtjason@gmail.com>
2021-07-31 12:11:10 -0700

    Add form validation

--- login/logic.js
+++ login/logic.js
@@ -3,8 +3,26 @@ function onSubmit(e) {
   e.preventDefault();
   const username = e.target.username.value.trim().toLowerCase();
   const password = e.target.password.value.trim();
+  const usernameEl = document.querySelector("#username");
+  const passwordEl = document.querySelector("#password");
+  const usernameError = document.querySelector(".username.invalid-feedback");
+  const passwordError = document.querySelector(".password.invalid-feedback");
 
-  console.log(password);
+  document.querySelectorAll(".is-invalid").forEach(item => item.classList.remove("is-invalid"));
+
+  if (!username.length) {
+    usernameError.innerHTML = "Please input your username.";
+    usernameEl.classList.add("is-invalid");
+    usernameEl.scrollIntoView({ behavior: 'smooth' });
+    return;
+  }
+
+  if (!password.length) {
+    passwordError.innerHTML = "Please input your password.";
+    passwordEl.classList.add("is-invalid");
+    passwordEl.scrollIntoView({ behavior: 'smooth' });
+    return;
+  }
 }
 
 function attachListeners() {
