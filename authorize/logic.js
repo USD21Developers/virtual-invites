cf08993376eeb2921454cdc88a078992ba42ce38
Jason McNeill <vrtjason@gmail.com>
2021-08-05 17:04:42 -0700

    Hide instructions within collapsible

--- authorize/logic.js
+++ authorize/logic.js
@@ -1,5 +1,14 @@
+function onInstructionsClicked() {
+  const instructionsButtonContainer = document.querySelector("#instructionsButtonContainer");
+  instructionsButtonContainer.classList.add("d-none");
+}
+
+function attachListeners() {
+  $("#instructions").on("show.bs.collapse", onInstructionsClicked);
+}
+
 function init() {
-  //
+  attachListeners();
 }
 
 init();
