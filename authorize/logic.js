6c4d37d602ccc09028bad023153d72d9693ece90
Jason McNeill <vrtjason@gmail.com>
2021-08-05 17:38:01 -0700

    Update UX

--- authorize/logic.js
+++ authorize/logic.js
@@ -1,14 +1,9 @@
-function onInstructionsClicked() {
-  const instructionsButtonContainer = document.querySelector("#instructionsButtonContainer");
-  instructionsButtonContainer.classList.add("d-none");
-}
-
 function attachListeners() {
-  $("#instructions").on("show.bs.collapse", onInstructionsClicked);
+  //
 }
 
 function init() {
-  attachListeners();
+  //
 }
 
 init();
