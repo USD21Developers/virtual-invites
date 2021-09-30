6577c368859e76c683519cf8045bfee4d8a470d6
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-09-30 14:54:25 -0700

    Enable primary language to be specified

--- events/add/logic.js
+++ events/add/logic.js
@@ -126,6 +126,83 @@ function onFrequencyChange(e) {
   }
 }
 
+function populateCountries() {
+  const country = document.querySelector("#country");
+  const lang = localStorage.getItem("lang") || "en";
+  const endpoint = `../../data/json/lang/${lang}/countries.json`;
+
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((countryItem) => {
+        const { name, alpha2 } = countryItem;
+        const option = document.createElement("option");
+        option.value = alpha2;
+        option.text = name;
+        country.add(option);
+      });
+      country.value = localStorage.getItem("country") || "us";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function populateLanguages() {
+  const languages = document.querySelector("#language");
+  const defaultLangIso =
+    JSON.parse(atob(localStorage.getItem("refreshToken").split(".")[1])).lang ||
+    "en";
+  /* const defaultLangIso = "es"; */
+  const endpoint = "../../data/json/languages.json";
+
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      if (defaultLangIso === "en") {
+        for (langIso in data) {
+          const option = document.createElement("option");
+          option.value = langIso;
+          option.text = data[langIso].name;
+          languages.add(option);
+        }
+      } else {
+        const nativeLangs = [];
+        for (langIso in data) {
+          nativeLangs.push({
+            iso: langIso,
+            name: data[langIso].nativeName,
+          });
+        }
+        nativeLangs.sort((a, b) => {
+          const lang1 = a.name.toLowerCase();
+          const lang2 = b.name.toLowerCase();
+
+          if (lang1 < lang2) {
+            return -1;
+          }
+
+          if (lang1 > lang2) {
+            return 1;
+          }
+
+          return 0;
+        });
+        nativeLangs.forEach((item) => {
+          const option = document.createElement("option");
+          option.value = item.iso;
+          option.text = item.name;
+          languages.add(option);
+        });
+      }
+      languages.value = defaultLangIso;
+      languages.parentElement.classList.add("has-value");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
 function showDetectLocationButton() {
   const isMobile = isMobileDevice();
   if (!isMobile) return;
@@ -149,6 +226,8 @@ function attachListeners() {
 
 async function init() {
   await populateContent();
+  populateCountries();
+  populateLanguages();
   attachListeners();
   showDetectLocationButton();
   initIntlTelInput();
