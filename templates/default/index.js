4357618c106fe36170a8999d37762927a3eaa4f5
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-10-19 15:34:44 -0700

    Add invite color themes

---
+++ templates/default/index.js
@@ -0,0 +1,14 @@
+function setTheme() {
+  const defaultTheme = "blue";
+  const color = localStorage.getItem("defaultTheme") || defaultTheme;
+  const theme = document.querySelector(".theme");
+
+  theme.setAttribute("data-theme", color);
+  theme.classList.remove("d-none");
+}
+
+function init() {
+  setTheme();
+}
+
+init();
