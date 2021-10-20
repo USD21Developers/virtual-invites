fa164e098275775afef09180aa4d6d000f07dbc5
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-10-20 13:46:42 -0700

    Add More Info modal

--- templates/default/index.js
+++ templates/default/index.js
@@ -7,8 +7,45 @@ function setTheme() {
   theme.classList.remove("d-none");
 }
 
+function onMoreInfoClicked(e) {
+  console.log(`onMoreInfoClicked`);
+  e.preventDefault();
+  const body = `
+    <p class="text-center">For more information, contact:</p>
+    <div class="row">
+      <div class="col col-sm-6 offset-sm-3 text-center">
+        <h4>Lance Jesterson</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-sm-6 offset-sm-3 text-center">
+        <div class="card">
+          <div class="card-body p-0">
+            <div class="list-group list-group-flush">
+              <a href="tel:+12133251382" class="list-group-item list-group-item-action">
+                Phone
+              </a>
+              <a href="sms:+12133251382" class="list-group-item list-group-item-action">SMS</a>
+              <a href="mailto:vrtjason@gmail.com" class="list-group-item list-group-item-action">E-mail</a>
+            </div>
+          </div>
+      </div>
+    </div>
+  `;
+  const title = "More Information";
+
+  showModal(body, title);
+}
+
+function addEventListeners() {
+  document
+    .querySelector("#moreinfo")
+    .addEventListener("click", onMoreInfoClicked);
+}
+
 function init() {
   setTheme();
+  addEventListeners();
 }
 
 init();
