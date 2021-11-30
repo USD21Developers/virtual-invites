4ebfd5ecbdc943c415f3e1736e7b06f4c752a89b
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-11-29 17:37:24 -0700

    Embed event description in preview

--- templates/default/index.js
+++ templates/default/index.js
@@ -7,54 +7,6 @@ function setTheme() {
   theme.classList.remove("d-none");
 }
 
-function onMoreInfoClicked(e) {
-  e.preventDefault();
-  const body = `
-    A Bible Talk is a casual, light-hearted discussion group that is open to anyone, regardless of their beliefs or prior experience with the Bible. We usually meet in a home or restaurant, but sometimes we meet in an open setting like a park.
-    <br />
-    <br />The discussion leader will focus on a Biblical topic and pertinent scriptures, and will engage the attendees with thought-provoking questions designed to get everyone thinking about those scriptures on a deeper level. The goal is to try to understand the scripture better through practical application, and to inspire attendees to pursue further Bible study. 
-    <hr />
-    <h3>Questions?</h3>
-    <div class="row">
-      <div class="col">
-        If you have any questions or feedback, please reach out below:
-      </div>
-    </div>
-    <div class="row my-3">
-      <div class="col col-sm-6 offset-sm-3 text-center">
-        <h4 class="m-0">Laurence Jesterson</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col col-sm-6 offset-sm-3 text-center">
-        <div class="card">
-          <div class="card-body p-0">
-            <div class="list-group list-group-flush">
-              <a href="tel:+12133251382" class="list-group-item list-group-item-action">
-                Phone call
-              </a>
-              <a href="sms:+12133251382" class="list-group-item list-group-item-action">
-                Text message
-              </a>
-              <a href="mailto:Laurence Jesterson &lt;vrtjason@gmail.com&gt;" class="list-group-item list-group-item-action">
-                E-mail
-              </a>
-            </div>
-          </div>
-      </div>
-    </div>
-  `;
-  const title = "More Information";
-
-  showModal(body, title);
-}
-
-function addEventListeners() {
-  document
-    .querySelector("#moreinfo")
-    .addEventListener("click", onMoreInfoClicked);
-}
-
 function init() {
   setTheme();
   addEventListeners();
