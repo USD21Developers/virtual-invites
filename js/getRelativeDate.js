465baec18a3187752f1e9d69682c322d56ed293c
Jason McNeill <vrtjason@gmail.com>
2022-03-28 13:00:21 -0700

    Refactor relative time format

--- js/getRelativeDate.js
+++ js/getRelativeDate.js
@@ -1,31 +1,37 @@
 function getRelativeDate(numDays = 0, locale = "en") {
-  const rtf = new Intl.RelativeTimeFormat(locale, {
-    localeMatcher: "best fit",
-    numeric: "auto",
-    style: "long"
-  });
+  try {
+    const rtf = new Intl.RelativeTimeFormat(locale, {
+      localeMatcher: "best fit",
+      numeric: "auto",
+      style: "long"
+    });
 
-  const numDaysAbsolute = Math.abs(numDays);
-  let unitType;
-  let unitQuantity;
+    const numDaysAbsolute = Math.abs(numDays);
+    let unitType;
+    let unitQuantity;
 
-  if (numDaysAbsolute >= 720) {
-    unitType = "year";
-    unitQuantity = Math.floor(numDaysAbsolute / 365);
-  } else if (numDaysAbsolute >= 60) {
-    unitType = "month";
-    unitQuantity = Math.floor(numDaysAbsolute / 30);
-  } else if (numDaysAbsolute >= 7) {
-    unitType = "week";
-    unitQuantity = Math.floor(numDaysAbsolute / 7);
-  } else {
-    unitType = "days";
-    unitQuantity = numDaysAbsolute;
+    if (numDaysAbsolute >= 720) {
+      unitType = "year";
+      unitQuantity = Math.floor(numDaysAbsolute / 365);
+    } else if (numDaysAbsolute >= 60) {
+      unitType = "month";
+      unitQuantity = Math.floor(numDaysAbsolute / 30);
+    } else if (numDaysAbsolute >= 7) {
+      unitType = "week";
+      unitQuantity = Math.floor(numDaysAbsolute / 7);
+    } else {
+      unitType = "days";
+      unitQuantity = numDaysAbsolute;
+    }
+
+    if (numDays < 0) {
+      unitQuantity = unitQuantity * -1;
+    }
+
+    return rtf.format(unitQuantity, unitType);
   }
-
-  if (numDays < 0) {
-    unitQuantity = unitQuantity * -1;
+  catch (err) {
+    console.log("Intl.RelativeTimeFormat is not supported. Switching to 'recently.'");
+    return getGlobalPhrase("recently");
   }
-
-  return rtf.format(unitQuantity, unitType);
 }
