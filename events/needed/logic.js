8685d649e686647044c83627f611ba55890f7d02
Jason McNeill <vrtjason@gmail.com>
2022-09-01 13:16:24 -0700

    Build out the page

--- events/needed/logic.js
+++ events/needed/logic.js
@@ -1,18 +1,5 @@
-function spinner(action="show", spinnerSelector="#pageSpinner", contentSelector="#pageContent") {
-    const spinnerEl = document.querySelector(spinnerSelector);
-    const contentEl = document.querySelector(contentSelector);
-    if (action === "hide") {
-        spinnerEl.classList.add("d-none");
-        contentEl.classList.remove("d-none");
-    } else if (action === "show") {
-        contentEl.classList.add("d-none");
-        spinnerEl.classList.remove("d-none");
-    }
-}
-
 async function init() {
     await populateContent();
-    spinner("hide");
     globalHidePageSpinner();
 }
   
