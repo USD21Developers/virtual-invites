4415b5aacbfd26052ac142b44b142e45d3f7aac0
Jason McNeill <vrtjason@gmail.com>
2022-09-08 13:05:31 -0700

    Build out Follow a User

--- follow/logic.js
+++ follow/logic.js
@@ -1,6 +1,43 @@
+async function onNameSearched(e) {
+    e.preventDefault();
+
+    e.target.querySelectorAll(".is-invalid").forEach(item => {
+        item.classList.remove("is-invalid");
+    });
+
+    const nameSearched = e.target.nameSearched.value || "";
+    const endpoint = `${getApiHost()}/user`;
+    const isBlank = nameSearched.trim().length ? false : true;
+    const controller = new AbortController();
+    const timeout = 8000;
+
+    if (isBlank) {
+        return e.target.querySelector("#nameSearched").classList.add("is-invalid");
+    }
+
+    // TODO:  send request to API
+}
+
+function onNameSearchInputted(e) {
+    const searchTerm = e.target.value || "";
+    const searchForm = document.querySelector("#formSearchByName");
+
+    if (searchTerm.trim().value !== "") {
+        searchForm.querySelectorAll(".is-invalid").forEach(item => {
+            item.classList.remove("is-invalid");
+        });
+    }
+}
+
+function attachListeners() {
+    document.querySelector("#formSearchByName").addEventListener("submit", onNameSearched);
+    document.querySelector("#nameSearched").addEventListener("input", onNameSearchInputted);
+}
+
 async function init() {
     await populateContent();
-    globalHidePageSpinner();  
+    globalHidePageSpinner();
+    attachListeners();
 }
 
 init();
