86726c4224bfa150f32947d6bdd9eb7214b1f4ea
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2022-12-23 10:21:54 -0700

    Add check for older browsers

---
+++ js/olderbrowsers.js
@@ -0,0 +1,20 @@
+function redirectOlderBrowsers() {
+  let isBrowserOK = true;
+
+  const supportsFetch = !!window.fetch;
+  const supportsCrypto = !!window.crypto;
+  const supportsCryptoSubtle = supportsCrypto && !!window.crypto.subtle;
+  const supportsKeyExport =
+    supportsCryptoSubtle && !!window.crypto.subtle.exportKey;
+
+  if (!supportsFetch) isBrowserOK = false;
+  if (!supportsCrypto) isBrowserOK = false;
+  if (!supportsCryptoSubtle) isBrowserOK = false;
+  if (!supportsKeyExport) isBrowserOK = false;
+
+  if (!isBrowserOK) {
+    window.location.href = "/upgradebrowser/";
+  }
+}
+
+redirectOlderBrowsers();
