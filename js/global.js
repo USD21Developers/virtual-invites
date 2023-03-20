fd5184c5ae9224fb199edb6b92f299476415f509
Jason McNeill <vrtjason@gmail.com>
2023-03-19 22:19:32 -0700

    Show profile photo in top nav

--- js/global.js
+++ js/global.js
@@ -900,6 +900,19 @@ function populateGlobalContent() {
             )?.content;
             if (matchedcontent) item.setAttribute("aria-label", matchedcontent);
           });
+        const profileImageNav = document.querySelector(".profileImageNav");
+        const { firstname, lastname, profilephoto } = JSON.parse(
+          atob(localStorage.getItem("refreshToken").split(".")[1])
+        );
+        const userFullName = `${firstname} ${lastname}`;
+        const profilePhotoNav = profilephoto.replace("400", "140");
+        const altText = getGlobalPhrase("profilePhotoAlt").replace(
+          "{name}",
+          userFullName
+        );
+        profileImageNav?.setAttribute("alt", altText);
+        profileImageNav?.setAttribute("src", profilePhotoNav);
+        profileImageNav?.parentElement.classList.remove("d-none");
         resolve();
       })
       .catch((err) => {
