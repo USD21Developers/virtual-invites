1e2d9381376262dc7aa91c20486c5f5889e9ab87
Jason McNeill <vrtjason@gmail.com>
2023-05-23 20:18:09 -0700

    Build out invite

---
+++ i/logic.js
@@ -0,0 +1,18 @@
+function populateTemplate(version = "default") {
+  const path = `../templates/${version}/index.html`;
+  fetch(path)
+    .then((res) => res.text())
+    .then((unparsed) => {
+      const parser = new DOMParser();
+      const parsed = parser.parseFromString(unparsed, "text/html");
+      const mainContent = parsed.querySelector(".container");
+      const mainEl = document.querySelector("main");
+      mainEl.appendChild(mainContent);
+    });
+}
+
+function init() {
+  populateTemplate();
+}
+
+init();
