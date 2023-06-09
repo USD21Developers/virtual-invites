a9ffbacf6af64bbefa8dbc2486a9ffe014a87245
Jason McNeill <vrtjason@gmail.com>
2023-06-08 17:44:14 -0700

    Add API call

--- i/logic.js
+++ i/logic.js
@@ -1,6 +1,52 @@
+async function getInvite() {
+  return new Promise((resolve, reject) => {
+    const inviteParts = window.location.hash.split("#")[1].split("/") || null;
+    if (!inviteParts) return reject();
+    if (!Array.isArray(inviteParts)) return reject();
+    if (!inviteParts.length) return reject();
+
+    let eventid = Number(inviteParts[0]) || null;
+    let userid = Number(inviteParts[1]) || null;
+    let recipientid = inviteParts[2] || null;
+
+    if (!eventid) return reject();
+
+    const endpoint = `${getApiHost()}/invite`;
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify({
+        eventid: eventid,
+        userid: userid,
+        recipientid: recipientid,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+}
+
 function hideSpinner() {
-  const el = document.querySelector("#pageSpinner");
-  el.classList.add("d-none");
+  const spinnerEl = document.querySelector("#pageSpinner");
+  const pageEl = document.querySelector("main");
+  spinnerEl.classList.add("d-none");
+  pageEl.classList.remove("d-none");
+}
+
+function showSpinner() {
+  const spinnerEl = document.querySelector("#pageSpinner");
+  const pageEl = document.querySelector("main");
+  spinnerEl.classList.remove("d-none");
+  pageEl.classList.add("d-none");
 }
 
 function populateTemplate(version = "default") {
@@ -14,7 +60,6 @@ function populateTemplate(version = "default") {
         const templateContent = parsed.querySelector(".container");
         const el = document.querySelector("main");
         el.appendChild(templateContent);
-        hideSpinner();
         resolve();
       });
   });
@@ -23,6 +68,7 @@ function populateTemplate(version = "default") {
 async function init() {
   await populateTemplate();
   await populateContent();
+  await getInvite();
 }
 
 init();
