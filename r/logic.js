48612edb1c53c3db120c13429fa63d4f062fd742
Jason McNeill <vrtjason@gmail.com>
2023-08-07 13:40:08 -0700

    Get invite from IDB and/or Sync

--- r/logic.js
+++ r/logic.js
@@ -1,4 +1,4 @@
-function getRecipient() {
+/* function getRecipient() {
   return new Promise(async (resolve, reject) => {
     const hash = window.location.hash;
     let recipientParts = hash.split("/") || null;
@@ -49,6 +49,52 @@ function getRecipient() {
 
         resolve(data.recipient);
       });
+  });
+} */
+
+function getRecipient() {
+  return new Promise(async (resolve, reject) => {
+    const hash = window.location.hash;
+    let recipientParts = hash.split("/") || null;
+    if (!recipientParts) {
+      return reject(new Error("Required URL parameters are missing"));
+    }
+    if (!Array.isArray(recipientParts)) {
+      return reject(new Error("URL parameters must be separated by slashes"));
+    }
+    if (!recipientParts.length) {
+      return reject(new Error("At least one URL parameter is required"));
+    }
+
+    const eventid = Number(recipientParts[1]) || null;
+    const userid = Number(recipientParts[2]) || null;
+    const recipientid = recipientParts[3] || null;
+
+    let invites = (await localforage.getItem("invites")) || null;
+
+    if (!invites) {
+      await syncInvites();
+      invites = await localforage.getItem("invites");
+    }
+
+    const invite = invites.find((item) => item.recipient.id === recipientid);
+
+    if (invite) {
+      renderRecipient(invite);
+      return resolve();
+    }
+
+    syncInvites().then(() => {
+      getRecipient();
+      return resolve();
+    });
+  });
+}
+
+function renderRecipient(invite) {
+  const { name } = invite.recipient;
+  document.querySelectorAll("[data-i18n='pagetitle']").forEach((item) => {
+    item.innerText = item.innerText.replaceAll("{RECIPIENT-NAME}", name);
   });
 }
 
