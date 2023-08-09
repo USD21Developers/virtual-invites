03ea1d1b1c43d3a8f58d0c83aff6d3028857ac39
Jason McNeill <vrtjason@gmail.com>
2023-08-08 17:52:48 -0700

    Build out front end

--- r/logic.js
+++ r/logic.js
@@ -1,57 +1,3 @@
-/* function getRecipient() {
-  return new Promise(async (resolve, reject) => {
-    const hash = window.location.hash;
-    let recipientParts = hash.split("/") || null;
-    if (!recipientParts) {
-      return reject(new Error("Required URL parameters are missing"));
-    }
-    if (!Array.isArray(recipientParts)) {
-      return reject(new Error("URL parameters must be separated by slashes"));
-    }
-    if (!recipientParts.length) {
-      return reject(new Error("At least one URL parameter is required"));
-    }
-
-    const eventid = Number(recipientParts[1]) || null;
-    const userid = Number(recipientParts[2]) || null;
-    const recipientid = recipientParts[3] || null;
-    const endpoint = `${getApiHost()}/recipient`;
-
-    // TODO:  validate notification token
-
-    const accessToken = await getAccessToken();
-
-    fetch(endpoint, {
-      mode: "cors",
-      method: "POST",
-      body: JSON.stringify({
-        eventid: eventid,
-        userid: userid,
-        recipientid: recipientid,
-      }),
-      headers: new Headers({
-        "Content-Type": "application/json",
-        authorization: `Bearer ${accessToken}`,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-
-        const { recipientname } = data.recipient;
-
-        document.querySelectorAll("[data-i18n='pagetitle']").forEach((item) => {
-          item.innerText = item.innerText.replaceAll(
-            "{RECIPIENT-NAME}",
-            recipientname
-          );
-        });
-
-        resolve(data.recipient);
-      });
-  });
-} */
-
 function getRecipient() {
   return new Promise(async (resolve, reject) => {
     const hash = window.location.hash;
@@ -92,7 +38,9 @@ function getRecipient() {
 }
 
 function renderRecipient(invite) {
-  const { name } = invite.recipient;
+  const { coords, eventid, recipient, sentvia, timezone, utctime } = invite;
+  const { email, id, name, sms } = recipient;
+
   document.querySelectorAll("[data-i18n='pagetitle']").forEach((item) => {
     item.innerText = item.innerText.replaceAll("{RECIPIENT-NAME}", name);
   });
