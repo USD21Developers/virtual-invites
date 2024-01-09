cc9a2ce5f2959423c67d203506288f658f6f56fd
Jason McNeill <vrtjason@gmail.com>
2024-01-09 00:17:50 -0700

    Add logic for Opening Page setting

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+async function redirectIfNecessary() {
+  let redirectedOnOpen = sessionStorage.getItem("redirectedOnOpen") || null;
+
+  if (redirectedOnOpen) {
+    return (window.location.href = redirectedOnOpen);
+  }
+
+  const settings = await localforage.getItem("settings");
+  const { openingPage } = settings;
+
+  switch (openingPage) {
+    case "home":
+      redirectedOnOpen = "/";
+      break;
+    case "send an invite":
+      redirectedOnOpen = "/send/";
+      break;
+    case "my invites":
+      redirectedOnOpen = "/invites/";
+      break;
+    case "follow up list":
+      redirectedOnOpen = "/followup/";
+      break;
+    default:
+      redirectedOnOpen = "/";
+  }
+
+  sessionStorage.setItem("redirectedOnOpen", redirectedOnOpen);
+  window.location.href = redirectedOnOpen;
+}
+
 async function init() {
+  redirectIfNecessary();
   await populateContent();
   globalHidePageSpinner();
   syncEvents();
