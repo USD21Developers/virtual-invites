a901b6a3a031c6b0c2d44c9f7a99ab1762a4a2d0
Jason McNeill <vrtjason@gmail.com>
2024-01-17 22:47:27 -0700

    Fix bug

--- js/global.js
+++ js/global.js
@@ -217,9 +217,7 @@ const invitesCrypto = {
 
     deserialize: (strKey) => {
       return new Promise(async (resolve, reject) => {
-        const array = atob(localStorage.getItem("datakey"))
-          .split(",")
-          .map(Number);
+        const array = atob(strKey).split(",").map(Number);
         const key = await window.crypto.subtle.importKey(
           "raw",
           new Uint8Array(array),
