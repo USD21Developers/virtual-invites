3158fa5fdeea8048762ae4e6d810524238c04f0c
Jason McNeill <vrtjason@gmail.com>
2024-02-05 12:48:03 -0700

    Improve UI

--- i/alt/logic.js
+++ i/alt/logic.js
@@ -1,5 +1,45 @@
+function toggleDetectLocationVisibility() {
+  const isMobile = isMobileDevice();
+  const detectMyLocationContainerEl = document.querySelector(
+    "#detectMyLocationContainer"
+  );
+
+  if (!isMobile) return;
+
+  detectMyLocationContainerEl.classList.remove("d-none");
+}
+
 function onDetectLocationClick(e) {
   e.preventDefault();
+
+  // if (!isMobileDevice()) return;
+
+  const onGeoLocationError = (err) => {
+    showToast(getPhrase("geocoordinatesErrorMessage"), 5000, "danger");
+    console.error(err);
+  };
+
+  const onGeoLocationSuccess = (pos) => {
+    const { latitude, longitude, timestamp } = pos.coords;
+    const mapCoordinates = `${latitude},${longitude}`;
+    const originLocationEl = document.querySelector("#originLocation");
+
+    originLocationEl.value = mapCoordinates;
+
+    showToast(getPhrase("geocoordinatesSuccessMessage"), 5000, "success");
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      onGeoLocationSuccess,
+      onGeoLocationError,
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }
 }
 
 function onSearch(e) {
@@ -18,6 +58,7 @@ function attachListeners() {
 async function init() {
   attachListeners();
   await populateContent();
+  toggleDetectLocationVisibility();
 }
 
 init();
