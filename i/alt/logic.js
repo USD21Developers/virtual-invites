c548e3d0e70708a263ddd706b40513d214b1fb8b
Jason McNeill <vrtjason@gmail.com>
2024-02-06 10:52:53 -0700

    Reduce time for toast message on screen

--- i/alt/logic.js
+++ i/alt/logic.js
@@ -29,7 +29,7 @@ function onDetectLocationClick(e) {
 
     if (promptAdvisory) promptAdvisory.classList.add("d-none");
 
-    showToast(getPhrase("geocoordinatesSuccessMessage"), 5000, "success");
+    showToast(getPhrase("geocoordinatesSuccessMessage"), 4000, "success");
   };
 
   if (navigator.geolocation) {
