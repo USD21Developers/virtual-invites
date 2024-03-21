8016d2b20be3c608e0b3feafc19e6c6cab39e737
Jason McNeill <vrtjason@gmail.com>
2024-03-21 16:32:01 -0700

    Fix bug

--- logout/logic.js
+++ logout/logic.js
@@ -1,20 +1,20 @@
 function init() {
-  debugger;
   const redirectOnLogin = sessionStorage.getItem("redirectOnLogin");
   const isFromHomeScreen = sessionStorage.getItem("isFromHomeScreen");
-  let newUrl = "../login/";
+  let loginURL = "../login/";
 
   clearStorage();
 
-  if (redirectOnLogin) {
+  if (!!isFromHomeScreen) {
+    loginURL = loginURL + "?utm_source=homescreen";
+    sessionStorage.setItem("isFromHomeScreen", "true");
+  }
+
+  if (!!redirectOnLogin) {
     sessionStorage.setItem("redirectOnLogin", redirectOnLogin);
   }
 
-  if (!!isFromHomeScreen) {
-    newUrl = newUrl + "?utm_source=homescreen";
-  }
-
-  window.location.href = newUrl;
+  window.location.href = loginURL;
 }
 
 init();
