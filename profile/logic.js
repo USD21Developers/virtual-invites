30c247a3035728c405b338378b7062894203a2d4
Jason McNeill <vrtjason@gmail.com>
2024-03-27 16:05:38 -0700

    Build out profile editing

--- profile/logic.js
+++ profile/logic.js
@@ -6,7 +6,105 @@ function openPhotoUploadModal(e) {
   e.preventDefault();
 }
 
+async function populateChurches() {
+  const churchDropdown = document.querySelector("#churchid");
+  const countryData = await getCountries(getLang());
+  const countries = countryData.names;
+  const churches = await getChurches();
+  let churchesHtml = "";
 
+  countries.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  for (let i = 0; i < countries.length; i++) {
+    const countryIso = countries[i].iso;
+    const countryName = countries[i].name;
+    const churchesInCountry = churches.filter(
+      (item) => item.country === countryIso
+    );
+    let churchesInCountryHtml = "";
+
+    if (!churchesInCountry.length) continue;
+
+    churchesInCountry.sort((a, b) => (a.place > b.place ? 1 : -1));
+    churchesInCountry.forEach((church) => {
+      const { country, id, name, place } = church;
+      if (!place) return;
+      const option = `<option value="${id}" data-name="${name}">${place}</option>`;
+      churchesInCountryHtml += option;
+    });
+
+    churchesInCountryHtml = `<optgroup label="${countryName}" data-country="${countryIso}">${churchesInCountryHtml}</optgroup>`;
+    if (churchesInCountryHtml.length) {
+      churchesHtml += churchesInCountryHtml;
+    }
+  }
+
+  churchDropdown.innerHTML = churchesHtml;
+
+  selectUserChurch();
+}
+
+function populateForm() {
+  return new Promise(async (resolve, reject) => {
+    const endpoint = `${getApiHost()}/user`;
+    const accessToken = await getAccessToken();
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data) return reject();
+        if (data.msgType !== "success") return reject();
+
+        const user = data.user;
+
+        const altText = getPhrase("profilePhoto")
+          .replaceAll("{FIRST-NAME}", user.firstname)
+          .replaceAll("{LAST-NAME}", user.lastname);
+
+        const photoLinkEl = document.querySelector("#profilePhoto");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", user.profilephoto);
+        img.setAttribute("alt", altText);
+        img.setAttribute("title", altText);
+        img.setAttribute("width", 140);
+        img.setAttribute("height", 140);
+
+        photoLinkEl.innerHTML = "";
+        photoLinkEl.appendChild(img);
+
+        document.querySelector("#username").innerHTML = user.username;
+        document.querySelector("#email").value = user.email;
+        document.querySelector("#firstname").value = user.firstname;
+        document.querySelector("#lastname").value = user.lastname;
+        document.querySelector("#churchid").value = user.churchid;
+
+        return resolve(user);
+      });
+  });
+}
+
+function selectUserChurch() {
+  const church = getStoredChurch(getUserChurchId());
+  const userChurchId = church.id;
+  const churchEl = document.querySelector("#churchid");
+
+  if (typeof userChurchId !== "number") return;
+
+  churchEl.value = userChurchId;
+
+  const churchName = church.name;
+  const churchNameEl = document.querySelector("#selectedChurchName");
+
+  churchNameEl.innerText = churchName;
+}
 
 function showProfilePhoto() {
   const refreshToken = JSON.parse(
@@ -41,16 +139,19 @@ function attachListeners() {
 }
 
 async function init() {
+  selectUserChurch();
+  populateChurches();
   await populateContent();
   showProfilePhoto();
+  await populateForm();
   attachListeners();
   globalHidePageSpinner();
 }
 
 /*
   TO DO:
-  1.  Populate churches field from localStorage (sorted by country)
-  2.  Sync churches with API asynchronously, with a callback that repopulates churches field again
+  1.  * DONE *  Populate churches field from localStorage (sorted by country)
+  2.  * DONE *  Sync churches with API asynchronously, with a callback that repopulates churches field again
   3.  Populate form fields
   4.  Enable changing profile photo
   5.  Create API to save changes
