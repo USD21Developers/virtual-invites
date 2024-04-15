d1ed1fe1b3fcaabadf3d502969a305d9a1ad3ab5
Jason McNeill <vrtjason@gmail.com>
2024-04-15 16:23:08 -0700

    Optimize SW

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,15 @@
 module.exports = {
   globDirectory: "./",
+  globIgnores: [
+    "sw.js",
+    "sw-src.js",
+    "workbox-config.js",
+    ".gitignore",
+    "node_modules/*",
+    "node_modules/**",
+    "js/*",
+    "js/**",
+  ],
   globPatterns: ["**/*.{html,js,css,map,png,gif,jpg,svg,ico,json}"],
   swSrc: "sw-src.js",
   swDest: "sw.js",
