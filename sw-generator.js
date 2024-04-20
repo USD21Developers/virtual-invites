bdb9e9dd350e21f9ccd1faf3e4379114b6363233
Jason McNeill <vrtjason@gmail.com>
2024-04-20 15:47:16 -0700

    Cache the manifest file

--- sw-generator.js
+++ sw-generator.js
@@ -2,7 +2,7 @@ const { injectManifest } = require("workbox-build");
 
 injectManifest({
   globDirectory: "./",
-  globPatterns: ["**/*.{html,css,js,svg,gif,png,jpg}"],
+  globPatterns: ["**/*.{html,css,js,svg,gif,png,jpg,webmanifest}"],
   globIgnores: [
     "sw-generator.js",
     "sw.js",
