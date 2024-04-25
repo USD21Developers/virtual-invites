f1ff7ba4a45278a6e85187cd0fcb39aa9721973a
Jason McNeill <vrtjason@gmail.com>
2024-04-25 13:40:14 -0700

    Wrap existing functionality in a promise

--- settings/logic.js
+++ settings/logic.js
@@ -1,66 +1,74 @@
 function getPushSubscription() {
-  if ("serviceWorker" in navigator && "PushManager" in window) {
-    navigator.serviceWorker.ready.then(function (registration) {
-      registration.pushManager
-        .getSubscription()
-        .then(function (subscription) {
-          if (subscription) {
-            // Push subscription already exists
-            return console.log(
-              "Push subscription already exists:",
-              subscription
-            );
-          }
+  return new Promise((resolve, reject) => {
+    if ("serviceWorker" in navigator && "PushManager" in window) {
+      navigator.serviceWorker.ready.then(function (registration) {
+        registration.pushManager
+          .getSubscription()
+          .then(function (subscription) {
+            if (subscription) {
+              // Push subscription already exists
+              console.log("Push subscription already exists:", subscription);
 
-          // No push subscription exists
-          console.log("Requesting push subscription...");
-
-          const urlBase64ToUint8Array = (base64String) => {
-            const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-            const base64 = (base64String + padding)
-              .replace(/\-/g, "+")
-              .replace(/_/g, "/");
-
-            const rawData = window.atob(base64);
-            const outputArray = new Uint8Array(rawData.length);
-
-            for (let i = 0; i < rawData.length; ++i) {
-              outputArray[i] = rawData.charCodeAt(i);
+              return resolve(subscription);
             }
-            return outputArray;
-          };
 
-          const VAPID_PUBLIC_KEY =
-            window.location.hostname === "localhost"
-              ? "BKnHjp6KUZvweNGC36UO8MnmydUW-xqgANz4K9UovnZpJXx4uWNa4aP1MJ_eFfj66s6kridOKRUA-Wy05FceJoY"
-              : "BLvcNxeIt_iASml9uC0DGSN0Akkeoc-QxoeGjz09FLu7G3YLxLTftw0pIKOqFtwdssmqQeWnKAfIAs98RmnQUP4";
+            // No push subscription exists
+            console.log("Requesting push subscription...");
 
-          const subscribeOptions = {
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
-          };
+            const urlBase64ToUint8Array = (base64String) => {
+              const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+              const base64 = (base64String + padding)
+                .replace(/\-/g, "+")
+                .replace(/_/g, "/");
 
-          return registration.pushManager.subscribe(subscribeOptions);
-        })
-        .then(function (pushSubscription) {
-          console.log(
-            "Push subscription obtained",
-            JSON.stringify(pushSubscription)
-          );
+              const rawData = window.atob(base64);
+              const outputArray = new Uint8Array(rawData.length);
 
-          $(".modal").modal("hide");
+              for (let i = 0; i < rawData.length; ++i) {
+                outputArray[i] = rawData.charCodeAt(i);
+              }
+              return outputArray;
+            };
 
-          syncPushSubscription();
+            const VAPID_PUBLIC_KEY =
+              window.location.hostname === "localhost"
+                ? "BKnHjp6KUZvweNGC36UO8MnmydUW-xqgANz4K9UovnZpJXx4uWNa4aP1MJ_eFfj66s6kridOKRUA-Wy05FceJoY"
+                : "BLvcNxeIt_iASml9uC0DGSN0Akkeoc-QxoeGjz09FLu7G3YLxLTftw0pIKOqFtwdssmqQeWnKAfIAs98RmnQUP4";
 
-          showToast(getPhrase("webPushNowAuthorized"), 5000, "success");
-        })
-        .catch(function (error) {
-          console.error("Error checking subscription:", error);
-        });
-    });
-  } else {
-    console.warn("Push messaging is not supported");
-  }
+            const subscribeOptions = {
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+            };
+
+            return registration.pushManager.subscribe(subscribeOptions);
+          })
+          .then(function (pushSubscription) {
+            console.log(
+              "Push subscription obtained",
+              JSON.stringify(pushSubscription)
+            );
+
+            $(".modal").modal("hide");
+
+            syncPushSubscription({
+              action: "create",
+            });
+
+            showToast(getPhrase("webPushNowAuthorized"), 5000, "success");
+
+            return resolve(pushSubscription);
+          })
+          .catch(function (error) {
+            console.error("Error checking subscription:", error);
+            return reject(error);
+          });
+      });
+    } else {
+      const msg = "Push messaging is not supported";
+      console.warn(msg);
+      return reject(new Error(msg));
+    }
+  });
 }
 
 function populateForm() {
