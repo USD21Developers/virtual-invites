f4abf7d605a2b0c0fa340371bb19cda140216ce5
Jason McNeill <vrtjason@gmail.com>
2024-04-25 15:25:58 -0700

    Update SW

--- sw.js
+++ sw.js
@@ -43,3 +43,79 @@ self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   event.waitUntil(clients.openWindow("https://example.com"));
 });
+
+function setCountry(country) {
+  localStorage.setItem("country", country);
+}
+
+function getAccessToken() {
+  return new Promise((resolve, reject) => {
+    const refreshToken = localStorage.getItem("refreshToken") || "";
+
+    if (!refreshToken.length) return reject("refresh token missing");
+
+    const isLocalhost =
+      location.hostname === "localhost" || location.hostname === "127.0.0.1"
+        ? true
+        : false;
+
+    const apiHost = isLocalhost
+      ? "http://localhost:4000/invites"
+      : "https://api.usd21.org/invites";
+
+    const endpoint = `${apiHost}/refresh-token`;
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify({
+        refreshToken: refreshToken,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        switch (data.msg) {
+          case "tokens renewed":
+            const { accessToken, refreshToken } = data;
+            localStorage.setItem("refreshToken", refreshToken);
+            const country =
+              JSON.parse(atob(accessToken.split(".")[1])).country || "us";
+            setCountry(country);
+            resolve(accessToken);
+            break;
+          default:
+            resolve("could not get access token");
+            break;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        return reject(error);
+      });
+  });
+}
+
+// If the push subscription changes (e.g. expires and is auto-renewed), update the it on the server
+self.addEventListener("pushsubscriptionchange", async (event) => {
+  const { oldSubscription, newSubscription } = event;
+  const endpoint = `${getApiHost}/update-web-push-subscription`;
+  const accessToken = await getAccessToken();
+
+  event.waitUntil(
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        oldSubscription: oldSubscription,
+        newSubscription: newSubscription,
+      }),
+    })
+  );
+});
