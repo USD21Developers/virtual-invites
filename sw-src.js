d57298003aeb9e38e0a3dacfd205c1143aa3e486
Jason McNeill <vrtjason@gmail.com>
2024-05-08 13:23:37 -0700

    Enable routes based on index.html as the default document in a directory

--- sw-src.js
+++ sw-src.js
@@ -1,7 +1,9 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js"
 );
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
+  directoryIndex: "index.html",
+});
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
