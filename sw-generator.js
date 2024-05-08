a26813046dfc6a7ae9d1a36128e40d31ec2e3786
Jason McNeill <vrtjason@gmail.com>
2024-05-08 11:37:36 -0700

    Add specific font references to precached assets

--- sw-generator.js
+++ sw-generator.js
@@ -2,7 +2,12 @@ const { injectManifest } = require("workbox-build");
 
 injectManifest({
   globDirectory: "./",
-  globPatterns: ["**/*.{html,css,js,map,svg,gif,png,jpg,webmanifest,json}"],
+  globPatterns: [
+    "**/*.{html,css,js,map,svg,gif,png,jpg,webmanifest,json}",
+    "**/_assets/fonts/Inter/Inter-VariableFont.ttf",
+    "**/_assets/fonts/MaterialIcons/material-icons.woff2",
+    "**/_assets/fonts/Oswald/Oswald-VariableFont.ttf",
+  ],
   globIgnores: [
     "sw-generator.js",
     "sw.js",
