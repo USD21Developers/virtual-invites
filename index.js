550cb15c3e33fdd5d23dc14602afd5d23e7b2b45
Jason McNeill <vrtjason@gmail.com>
2024-05-27 11:52:36 -0700

    FIx bug

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ async function redirectIfNecessary() {
   const launchedFromHomescreen =
     window.location.search === "?utm_source=homescreen" ? true : false;
   const settings = await localforage.getItem("settings");
-  const { openingPage } = settings;
-  let redirectUrl = "/";
+  let redirectUrl;
 
-  if (launchedFromHomescreen) {
-    switch (openingPage) {
-      case "home":
-        break;
-      case "send an invite":
-        redirectUrl = "/send/";
-        break;
-      case "my invites":
-        redirectUrl = "/invites/";
-        break;
-      case "follow up list":
-        redirectUrl = "/followup/";
-        break;
-      default:
-        break;
-    }
+  if (!launchedFromHomescreen) return;
+  if (!settings) return;
+  if (!settings.openingPage) return;
 
+  switch (settings.openingPage) {
+    case "home":
+      break;
+    case "send an invite":
+      redirectUrl = "/send/";
+      break;
+    case "my invites":
+      redirectUrl = "/invites/";
+      break;
+    case "follow up list":
+      redirectUrl = "/followup/";
+      break;
+    default:
+      break;
+  }
+
+  if (redirectUrl) {
     window.location.href = redirectUrl;
   }
 }
