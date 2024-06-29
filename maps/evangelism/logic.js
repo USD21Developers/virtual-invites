4830e66a5c194f94715bedfc478d1557eb71d108
Jason McNeill <vrtjason@gmail.com>
2024-06-29 10:54:26 -0700

    Improve UI

--- maps/evangelism/logic.js
+++ maps/evangelism/logic.js
@@ -2,10 +2,10 @@ function toggleFromDateTime(e) {
   const fromDateTimeContainerEl = document.querySelector(
     "#fromDateTimeContainer"
   );
-  const isSpecificDateTimeChecked =
+  const isSpecificDateTimeSelected =
     e.target.value === "specificDateTime" ? true : false;
 
-  if (isSpecificDateTimeChecked) {
+  if (isSpecificDateTimeSelected) {
     fromDateTimeContainerEl.classList.remove("d-none");
   } else {
     fromDateTimeContainerEl.classList.add("d-none");
@@ -14,8 +14,20 @@ function toggleFromDateTime(e) {
 
 function toggleToDateTime(e) {
   const toDateTimeContainerEl = document.querySelector("#toDateTimeContainer");
+  const isSpecificDateTimeSelected =
+    e.target.value === "specificDateTime" ? true : false;
+
+  if (isSpecificDateTimeSelected) {
+    toDateTimeContainerEl.classList.remove("d-none");
+  } else {
+    toDateTimeContainerEl.classList.add("d-none");
+  }
+}
+
+function toggleToDateTime(e) {
+  const toDateTimeContainerEl = document.querySelector("#toDateTimeContainer");
   const isSpecificDateTimeChecked =
-    e.target.value === "previous" ? true : false;
+    e.target.value === "specificDateTime" ? true : false;
 
   if (isSpecificDateTimeChecked) {
     toDateTimeContainerEl.classList.remove("d-none");
@@ -24,16 +36,19 @@ function toggleToDateTime(e) {
   }
 }
 
+function onSubmit(e) {
+  e.preventDefault();
+  showModal("This feature is coming soon!", "Under construction");
+}
+
 function addEventListeners() {
   document
     .querySelector("#fromPresets")
     .addEventListener("change", toggleFromDateTime);
   document
-    .querySelector("#toDateTimeNow")
-    .addEventListener("click", toggleToDateTime);
-  document
-    .querySelector("#toDateTimePrevious")
-    .addEventListener("click", toggleToDateTime);
+    .querySelector("#toPresets")
+    .addEventListener("change", toggleToDateTime);
+  document.querySelector("#mapForm").addEventListener("submit", onSubmit);
 }
 
 async function init() {
