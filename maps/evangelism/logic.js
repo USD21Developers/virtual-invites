49e570f5764e7c9bf879ce14e27c83eaca62d881
Jason McNeill <vrtjason@gmail.com>
2024-06-29 11:21:30 -0700

    Persist From and To datetime values

--- maps/evangelism/logic.js
+++ maps/evangelism/logic.js
@@ -1,9 +1,47 @@
+function populateDefaultValues() {
+  const fromSelectionStored = localStorage.getItem("mapEvangelismFrom");
+  const toSelectionStored = localStorage.getItem("mapEvangelismTo");
+  const fromPresetsEl = document.querySelector("#fromPresets");
+  const toPresetsEl = document.querySelector("#toPresets");
+  const fromSpecificDateTimeEl = document.querySelector(
+    "#fromDateTimeContainer"
+  );
+  const toSpecificDateTimeEl = document.querySelector("#toDateTimeContainer");
+
+  if (fromSelectionStored && fromSelectionStored.length) {
+    fromPresets.value = fromSelectionStored;
+  } else {
+    localStorage.setItem("mapEvangelismFrom", fromPresetsEl.value);
+  }
+
+  if (toSelectionStored && toSelectionStored.length) {
+    toPresets.value = toSelectionStored;
+  } else {
+    localStorage.setItem("mapEvangelismTo", toPresetsEl.value);
+  }
+
+  if (fromPresetsEl.value === "specificDateTime") {
+    fromSpecificDateTimeEl.classList.remove("d-none");
+  } else {
+    fromSpecificDateTimeEl.classList.add("d-none");
+  }
+
+  if (toPresetsEl.value === "specificDateTime") {
+    toSpecificDateTimeEl.classList.remove("d-none");
+  } else {
+    toSpecificDateTimeEl.classList.add("d-none");
+  }
+}
+
 function toggleFromDateTime(e) {
   const fromDateTimeContainerEl = document.querySelector(
     "#fromDateTimeContainer"
   );
   const isSpecificDateTimeSelected =
     e.target.value === "specificDateTime" ? true : false;
+  const currentSelection = e.target.value;
+
+  localStorage.setItem("mapEvangelismFrom", currentSelection);
 
   if (isSpecificDateTimeSelected) {
     fromDateTimeContainerEl.classList.remove("d-none");
@@ -14,20 +52,11 @@ function toggleFromDateTime(e) {
 
 function toggleToDateTime(e) {
   const toDateTimeContainerEl = document.querySelector("#toDateTimeContainer");
-  const isSpecificDateTimeSelected =
-    e.target.value === "specificDateTime" ? true : false;
-
-  if (isSpecificDateTimeSelected) {
-    toDateTimeContainerEl.classList.remove("d-none");
-  } else {
-    toDateTimeContainerEl.classList.add("d-none");
-  }
-}
-
-function toggleToDateTime(e) {
-  const toDateTimeContainerEl = document.querySelector("#toDateTimeContainer");
   const isSpecificDateTimeChecked =
     e.target.value === "specificDateTime" ? true : false;
+  const currentSelection = e.target.value;
+
+  localStorage.setItem("mapEvangelismTo", currentSelection);
 
   if (isSpecificDateTimeChecked) {
     toDateTimeContainerEl.classList.remove("d-none");
@@ -54,6 +83,7 @@ function addEventListeners() {
 async function init() {
   await populateContent();
   addEventListeners();
+  populateDefaultValues();
   globalHidePageSpinner();
 }
 
