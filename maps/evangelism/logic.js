514e8e0ac276dfea94565a8977366c2a0f96d36b
Jason McNeill <vrtjason@gmail.com>
2024-06-28 17:51:44 -0700

    Build out form

--- maps/evangelism/logic.js
+++ maps/evangelism/logic.js
@@ -1,5 +1,35 @@
+function populateDefaultDates() {
+  const toNowEl = document.querySelector("#toNow");
+  const toDateEl = document.querySelector("#toDate");
+  const toTimeEl = document.querySelector("#toTime");
+  const dateNow = moment().format("YYYY-MM-DD");
+  const timeNow = moment().format("HH:mm:ss");
+
+  if (toNowEl.checked) {
+    toDateEl.value = dateNow;
+    toTimeEl.value = timeNow;
+  }
+}
+
+function toggleToDateTime(e) {
+  if (e.target.checked) {
+    populateDefaultDates();
+  } else {
+    const toDateEl = document.querySelector("#toDate");
+    const toTimeEl = document.querySelector("#toTime");
+    toDateEl.value = "";
+    toTimeEl.value = "";
+  }
+}
+
+function addEventListeners() {
+  document.querySelector("#toNow").addEventListener("click", toggleToDateTime);
+}
+
 async function init() {
   await populateContent();
+  populateDefaultDates();
+  addEventListeners();
   globalHidePageSpinner();
 }
 
