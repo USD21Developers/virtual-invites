ca8dbb5d415fe4521c1f087d9a5cdab00a550dcd
Jason McNeill <vrtjason@gmail.com>
2024-06-29 17:59:34 -0700

    Persist values of specific date and specific time

--- maps/evangelism/logic.js
+++ maps/evangelism/logic.js
@@ -7,15 +7,23 @@ function populateDefaultValues() {
     "#fromDateTimeContainer"
   );
   const toSpecificDateTimeEl = document.querySelector("#toDateTimeContainer");
+  const fromDateEl = document.querySelector("#fromDate");
+  const fromTimeEl = document.querySelector("#fromTime");
+  const toDateEl = document.querySelector("#toDate");
+  const toTimeEl = document.querySelector("#toTimeEl");
+  const fromDateStored = localStorage.getItem("mapEvangelismFromDate");
+  const fromTimeStored = localStorage.getItem("mapEvangelismFromTime");
+  const toDateStored = localStorage.getItem("mapEvangelismToDate");
+  const toTimeStored = localStorage.getItem("mapEvangelismToTimeStored");
 
   if (fromSelectionStored && fromSelectionStored.length) {
-    fromPresets.value = fromSelectionStored;
+    fromPresetsEl.value = fromSelectionStored;
   } else {
     localStorage.setItem("mapEvangelismFrom", fromPresetsEl.value);
   }
 
   if (toSelectionStored && toSelectionStored.length) {
-    toPresets.value = toSelectionStored;
+    toPresetsEl.value = toSelectionStored;
   } else {
     localStorage.setItem("mapEvangelismTo", toPresetsEl.value);
   }
@@ -30,6 +38,30 @@ function populateDefaultValues() {
     toSpecificDateTimeEl.classList.remove("d-none");
   } else {
     toSpecificDateTimeEl.classList.add("d-none");
+  }
+
+  if (fromDateStored && fromDateStored.length) {
+    fromDateEl.value = fromDateStored;
+  } else {
+    localStorage.setItem("mapEvangelismFromDate", fromDateEl.value);
+  }
+
+  if (fromTimeStored && fromTimeStored.length) {
+    fromTimeEl.value = fromTimeStored;
+  } else {
+    localStorage.setItem("mapEvangelismFromTime", fromTimeEl.value);
+  }
+
+  if (toDateStored && toDateStored.length) {
+    toDateEl.value = toDateStored;
+  } else {
+    localStorage.setItem("mapEvangelismToDate", toDateEl.value);
+  }
+
+  if (toTimeStored && toTimeStored.length) {
+    toTimeEl.value = toTimeStored;
+  } else {
+    localStorage.setItem("mapEvangelismToTime", toTimeEl.value);
   }
 }
 
@@ -65,6 +97,34 @@ function toggleToDateTime(e) {
   }
 }
 
+function onChangeFromDate(e) {
+  const newValue = e.target.value;
+  if (newValue && newValue.length) {
+    localStorage.setItem("mapEvangelismFromDate", newValue);
+  }
+}
+
+function onChangeFromTime(e) {
+  const newValue = e.target.value;
+  if (newValue && newValue.length) {
+    localStorage.setItem("mapEvangelismFromTime", newValue);
+  }
+}
+
+function onChangeToDate(e) {
+  const newValue = e.target.value;
+  if (newValue && newValue.length) {
+    localStorage.setItem("mapEvangelismToDate", newValue);
+  }
+}
+
+function onChangeToTime(e) {
+  const newValue = e.target.value;
+  if (newValue && newValue.length) {
+    localStorage.setItem("mapEvangelismToTime", newValue);
+  }
+}
+
 function onSubmit(e) {
   e.preventDefault();
   showModal("This feature is coming soon!", "Under construction");
@@ -77,6 +137,15 @@ function addEventListeners() {
   document
     .querySelector("#toPresets")
     .addEventListener("change", toggleToDateTime);
+  document
+    .querySelector("#fromDate")
+    .addEventListener("change", onChangeFromDate);
+  document
+    .querySelector("#fromTime")
+    .addEventListener("change", onChangeFromTime);
+  document.querySelector("#toDate").addEventListener("change", onChangeToDate);
+  document.querySelector("#toTime").addEventListener("change", onChangeToTime);
+
   document.querySelector("#mapForm").addEventListener("submit", onSubmit);
 }
 
