ac4896d70185186e621529dafdf55d44a02b8695
Jason McNeill <vrtjason@gmail.com>
2024-08-19 07:30:16 -0700

    Build out content

--- authorize/user/logic.js
+++ authorize/user/logic.js
@@ -1,3 +1,13 @@
+let registrant;
+
+function populateParagraph1() {
+  const p1El = document.querySelector("#p1");
+  let p1Phrase =
+    registrant.gender === "male" ? getPhrase("p1Male") : getPhrase("p1Female");
+  p1Phrase = p1Phrase.replaceAll("{FIRST-NAME}", registrant.firstname);
+  p1El.innerHTML = p1Phrase;
+}
+
 async function populateRegistrant() {
   const churches = await getChurches();
   const userid = Number(window.location.hash.split("/")[1]);
@@ -16,6 +26,8 @@ async function populateRegistrant() {
     .then((data) => {
       return data.profile;
     });
+
+  registrant = user;
 
   const name = `${user.firstname} ${user.lastname}`;
   const church = churches.find((item) => item.id === user.churchid);
@@ -50,13 +62,40 @@ async function populateRegistrant() {
   document.querySelector("#churchname").innerHTML = church.name;
 }
 
+function onWhyAuthorizeClicked(e) {
+  e.preventDefault();
+  const authorizeModalEl = document.querySelector("#whyAuthorizeModal");
+  $("#whyAuthorizeModal").modal();
+}
+
+async function populateWhyAuthorize() {
+  const url = `../../authorize/i18n/${getLang()}.json`;
+  const phraseJSON = await fetch(url).then((res) => res.json());
+  const modalEl = document.querySelector("#whyAuthorizeModal");
+  const linkEl = document.querySelector("#whyAuthorizeLink");
+  const titleText = getPhrase("whyAuthorizeHeading", phraseJSON);
+  const p1Text = getPhrase("whyAuthorizeP1", phraseJSON);
+  const p2Text = getPhrase("whyAuthorizeP2", phraseJSON);
+
+  linkEl.innerHTML = titleText;
+  modalEl.querySelector(".modal-title").innerHTML = titleText;
+  modalEl.querySelector("[data-i18n='whyAuthorizeP1']").innerHTML = p1Text;
+  modalEl.querySelector("[data-i18n='whyAuthorizeP2']").innerHTML = p2Text;
+}
+
 function attachListeners() {
   window.addEventListener("hashchange", (e) => window.location.reload());
+
+  document
+    .querySelector("#whyAuthorizeLink")
+    .addEventListener("click", onWhyAuthorizeClicked);
 }
 
 async function init() {
   await populateContent();
   await populateRegistrant();
+  populateParagraph1();
+  await populateWhyAuthorize();
   attachListeners();
   globalHidePageSpinner();
 }
