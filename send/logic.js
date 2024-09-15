f215e0b343c12483958bfbf949d0304fff5be95d
Jason McNeill <vrtjason@gmail.com>
2024-09-15 06:47:52 -0700

    Fix bug

--- send/logic.js
+++ send/logic.js
@@ -1231,7 +1231,7 @@ function setEventListeners() {
 async function init() {
   setRecipientID();
   await checkForEvents();
-  clearForm();
+  // clearForm();
   await populateContent();
   populateSaveButtonData();
   await loadEvents();
