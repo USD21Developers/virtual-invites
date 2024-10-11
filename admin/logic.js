54e32adf6dd1037c6903b29e2c194c7a6eb1a1db
Jason McNeill <vrtjason@gmail.com>
2024-10-11 15:38:03 -0700

    Build out UI for admin

--- admin/logic.js
+++ admin/logic.js
@@ -1,6 +1,92 @@
+async function populateChurches() {
+  const churchDropdown = document.querySelector("#churchid");
+  const countryData = await getCountries(getLang());
+  const countries = countryData.names;
+  const churches = await getChurches();
+  let churchesHtml = "";
+
+  countries.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  for (let i = 0; i < countries.length; i++) {
+    const countryIso = countries[i].iso;
+    const countryName = countries[i].name;
+    const churchesInCountry = churches.filter(
+      (item) => item.country === countryIso
+    );
+    let churchesInCountryHtml = "";
+
+    if (!churchesInCountry.length) continue;
+
+    churchesInCountry.sort((a, b) => (a.place > b.place ? 1 : -1));
+    churchesInCountry.forEach((church) => {
+      const { country, id, name, place } = church;
+      if (!place) return;
+      const option = `<option value="${id}" data-name="${name}">${place}</option>`;
+      churchesInCountryHtml += option;
+    });
+
+    churchesInCountryHtml = `<optgroup label="${countryName}" data-country="${countryIso}">${churchesInCountryHtml}</optgroup>`;
+    if (churchesInCountryHtml.length) {
+      churchesHtml += churchesInCountryHtml;
+    }
+  }
+
+  churchDropdown.innerHTML = churchesHtml;
+
+  selectUserChurch();
+
+  syncChurches();
+}
+
+async function selectUserChurch() {
+  const myUserId = getUserId();
+  const myChurchId = await getUserChurchId(myUserId);
+  const church = getStoredChurch(myChurchId);
+  const userChurchId = church.id;
+  const churchEl = document.querySelector("#churchid");
+
+  if (typeof userChurchId !== "number") return;
+
+  churchEl.value = userChurchId;
+
+  const churchName = church.name;
+  const churchNameEl = document.querySelector("#selectedChurchName");
+
+  churchNameEl.innerText = churchName;
+}
+
+function onUserSearch(e) {
+  e.preventDefault();
+
+  const churchid = document.querySelector("#churchid").value;
+  const firstname = document.querySelector("#firstname").value;
+  const lastname = document.querySelector("#lastname").value;
+  const endpoint = `${getApiHost()}/get-user`;
+
+  document
+    .querySelectorAll(".is-invalid")
+    .forEach((item) => item.classList.remove("is-invalid"));
+
+  if (isNaN(churchid)) {
+    formError("#churchid", getPhrase("errorChurchIsRequired"));
+  }
+
+  if (firstname.trim() === "" && lastname.trim() === "") {
+    document.querySelector("#firstname").classList.add("is-invalid");
+    formError("#firstname", getPhrase("errorNameIsRequired"))
+  }
+}
+
+function attachListeners() {
+  document
+    .querySelector("#finduserform")
+    .addEventListener("submit", onUserSearch);
+}
 
 async function init() {
   await populateContent();
+  populateChurches();
+  attachListeners();
   globalHidePageSpinner();
 }
 
