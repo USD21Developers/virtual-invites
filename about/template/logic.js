d52d54cb5f4c0f2b8db5a9bd0e10b6649c61b140
Jason McNeill <vrtjason@gmail.com>
2025-01-03 09:50:54 -0700

    Hide promotion of registration for already-registered users

--- about/template/logic.js
+++ about/template/logic.js
@@ -1,5 +1,16 @@
+function toggleRegistrationPromotion() {
+  const refreshTokenStored = localStorage.getItem("refreshToken");
+
+  if (!refreshTokenStored) {
+    document.querySelectorAll(".promotesRegistration").forEach((item) => {
+      item.classList.remove("d-none");
+    });
+  }
+}
+
 async function init() {
   await populateContent();
+  toggleRegistrationPromotion();
   globalHidePageSpinner();
 }
 
