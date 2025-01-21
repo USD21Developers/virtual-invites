8865d849a0bc03e974cb8c9c5e16eb9484d733df
Jason McNeill <vrtjason@gmail.com>
2025-01-21 12:31:01 -0700

    Build out UI

--- admin/users/list/logic.js
+++ admin/users/list/logic.js
@@ -1,5 +1,24 @@
+function showChurch() {
+  return new Promise(async (resolve, reject) => {
+    const churchNameEl = document.querySelector("#churchName");
+    const churchIdParam = Number(window.location.search.split("=")[1]);
+    const churches = await getChurches();
+    const church = churches.find((item) => item.id === churchIdParam);
+
+    if (!church) {
+      return reject(new Error("unable to find church"));
+    }
+
+    churchNameEl.innerHTML = church.name;
+
+    return resolve(church.name);
+  });
+}
+
 async function init() {
   await populateContent();
+  syncChurches();
+  await showChurch();
   globalHidePageSpinner();
 }
 
