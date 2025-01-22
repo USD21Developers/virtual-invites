98e7ab5b18ffc129c9a65fbc24ca56cfce34a9b3
Jason McNeill <vrtjason@gmail.com>
2025-01-22 13:19:33 -0700

    Build out UX

--- admin/users/list/logic.js
+++ admin/users/list/logic.js
@@ -1,4 +1,8 @@
-function showChurch() {
+function renderUsers(users) {
+  // TODO
+}
+
+function showChurchName() {
   return new Promise(async (resolve, reject) => {
     const churchNameEl = document.querySelector("#churchName");
     const churchIdParam = Number(window.location.search.split("=")[1]);
@@ -15,10 +19,54 @@ function showChurch() {
   });
 }
 
+function showChurchMembers() {
+  return new Promise(async (resolve, reject) => {
+    const churchid = Number(window.location.search.split("=")[1]);
+    const endpoint = `${getApiHost()}/users-church`;
+    const accessToken = await getAccessToken();
+
+    if (typeof churchid !== "number")
+      return reject(new Error("churchid in URL parameters must be a number"));
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "post",
+      body: JSON.stringify({
+        churchid: churchid,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data) {
+          return reject(new Error("API did not return data"));
+        }
+
+        const { users } = data;
+
+        if (!users) {
+          return reject(new Error("users were missing in API response"));
+        }
+
+        renderUsers(users);
+
+        return resolve(users);
+      })
+      .catch((error) => {
+        console.error(error);
+        return reject(error);
+      });
+  });
+}
+
 async function init() {
   await populateContent();
   syncChurches();
-  await showChurch();
+  await showChurchName();
+  await showChurchMembers();
   globalHidePageSpinner();
 }
 
