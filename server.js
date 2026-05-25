505306d52106113d0fadf69571289fe86cd4e99b
Jason McNeill <vrtjason@gmail.com>
2026-05-25 11:33:02 -0700

    Add route for invite

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+require("dotenv").config();
+
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // 1. Define your specific dynamic EJS route first
-app.get("/special-route", (req, res) => {
-  // Pass any local data you want to render in your EJS template
-  res.render("dynamic-route", { title: "Dynamic Invitation" });
+app.get("/i/:eventid/:userid/:recipientid", (req, res) => {
+  const { eventid, userid, recipientid } = req.params;
+
+  res.render("invite", {
+    title: "Invitation",
+    eventid: eventid,
+    userid: userid,
+    recipientid: recipientid,
+  });
 });
 
 // 2. Serve everything else out of the public folder statically
